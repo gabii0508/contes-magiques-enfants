99a638ddad7cc3b7f36ea8f60a94288c5de86f97
gabii0805 <gabygui27@gmail.com>
2024-11-12 10:23:48 +0100

    init back

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+// Importer Express
+const express = require('express');
+
+// Créer une instance de l'application Express
+const app = express();
+
+// Définir le port de l'application
+const PORT = 5000;
+
+// Tableau de personnages, lieux et objets magiques
+const personnages = [
+    'Un lion courageux',
+    'Une fée bienveillante',
+    'Un dragon gentil'
+];
+const lieux = [
+    'dans une forêt enchantée',
+    'une montagne magique',
+    'dans l espace'
+];
+const objets = [
+    'une baguette magique',
+    'un livre de sorts',
+    'une pierre précieuse magique'
+];
